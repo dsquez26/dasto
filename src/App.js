35cf2104e3145e1d35b219390778384967fa1036
Davidvazquez1026@gmail.com <davidvazquez@gmail.com>
2022-11-16 22:13:16 -0600

    added sign-in button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 import Home from "./routes/home/home.component";
 
@@ -13,6 +14,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='/signIn' element={<SignIn />} />
       </Route>
     </Routes>
   );
