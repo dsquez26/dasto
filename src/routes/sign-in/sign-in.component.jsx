b790cf246631d9a35c95400c55e239193c0cebed
Davidvazquez1026@gmail.com <davidvazquez@gmail.com>
2022-11-20 12:39:15 -0600

    firebase auth hooked up and creating users

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,20 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase.utils"
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase.utils";
 
 const SignIn = () => {
-  const logGoogleUser = async ()=> {
-    const { user } = await signInWithGooglePopup()
-    createUserDocumentFromAuth(user)
-  }
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
 
   return (
     <div>
-    <div>Sign-In Page</div>
-     <button onClick={logGoogleUser}>
-      Sign-In with Google Pop-up
-     </button>
+      <div>Sign-In Page</div>
+      <button onClick={logGoogleUser}>Sign-In with Google</button>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,20 +1,13 @@
 import { initializeApp } from "firebase/app";
 
-
 import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-  
 } from "firebase/auth";
 
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc
-} from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC9BhWVfG8Co36UiVvPwXUuMx1WxoSVoMs",
@@ -39,6 +32,23 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, 'users', userAuth.uid);
-  console.log("HELLLOOOOOOO", userDocRef)
-}
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  //if user data exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+  //if user data does not exist
+  //create/set the document with the data from userAuth in my collection
+
+  //return userDocRef
+  return userDocRef;
+};
